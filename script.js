156b771d76ec6c2d1cbb619842bd3884cc1bc622
TapishKhari <contact.tapish@gmail.com>
2022-09-12 19:44:37 +0530

    Updated the HTML, JS, CSS files

---
+++ script.js
@@ -0,0 +1,13 @@
+const button = document.getElementById('menuicon');
+const website=document.querySelector('.website');
+const close= document.querySelector('.close');
+
+
+
+button.addEventListener('click',() =>{
+    website.classList.toggle("active");
+});
+
+close.addEventListener('click',() => {
+    website.classList.remove("active");
+});
